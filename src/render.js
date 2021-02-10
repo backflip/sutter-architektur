0be2fe151227798b79a3881320dae9cc919c3190
Thomas Jaggi <thomas@responsive.ch>
2021-02-10 19:16:28 +0100

    fix: include resolving issue on server

--- src/render.js
+++ src/render.js
@@ -6,6 +6,9 @@ const templatePath = path.join(__dirname, "./index.html");
 const template = fs.readFileSync(templatePath, "utf8");
 
 export function renderLandingPage(data = {}) {
+  // Workaround for apparent timing/discovery issue when resolving include paths
+  console.log(fs.existsSync(path.join(__dirname, "./analytics.html")));
+
   const html = ejs.render(template, data, {
     views: [__dirname],
   });
